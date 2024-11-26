1a4e648562b3dd8c7de86f55cb565ea401c5ae7e
Youssef Magdy <yma.youssef.21@gmail.com>
2024-11-26 22:55:15 +0200

    added design styles config for colors-fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      "nunito-sans": '"Nunito Sans", sans-serif',
+    },
+    fontWeight: {
+      light: 300,
+      bold: 600,
+      extraBold: 800,
+    },
+    colors: {
+      transparent: "transparent",
+      white: "white",
+      black: "black",
+      light: {
+        primary: "hsl(0, 0%, 98%)",
+        accent: "hsl(0, 0%, 100%)",
+        text: "hsl(200, 15%, 8%)",
+      },
+      dark: {
+        primary: "hsl(207, 26%, 17%)",
+        accent: "hsl(209, 23%, 22%)",
+        text: "hsl(0, 0%, 100%)",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
 };

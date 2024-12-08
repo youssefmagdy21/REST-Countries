6ea21e46a5e3be23b525b2234e4c81837fb1456b
Youssef Magdy <yma.youssef.21@gmail.com>
2024-12-08 20:53:39 +0200

    finished the header component - light mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,12 @@ import { MdOutlineDarkMode } from "react-icons/md";
 
 function Header() {
   return (
-    <header className="bg-light-accent text-light-text drop-shadow">
-      {/* container */}
-      <div className="flex items-center justify-between px-4 py-14 sm:px-16 sm:py-7">
-        <h1 className="cursor-default text-lg font-extraBold sm:text-2xl">
+    <header className="bg-light-accent text-light-text shadow">
+      <div className="flex items-center justify-between px-4 py-[30px] md:px-[4.75rem] md:py-6">
+        <h1 className="cursor-default text-base/5 font-extraBold md:text-xl/8">
           Where in the world?
         </h1>
-        <button className="group flex items-center gap-1 text-base font-bold transition-all duration-300 ease-in-out hover:-translate-y-1 hover:opacity-80">
+        <button className="group flex items-center gap-1 text-xs font-bold transition-all duration-300 ease-in-out hover:-translate-y-1 hover:opacity-80 md:text-sm">
           <MdOutlineDarkMode className="transition-transform duration-300 ease-in-out group-hover:-rotate-90" />
           Dark Mode
         </button>

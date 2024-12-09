ce8bb4f01bba2729b72c0488b9f54af9356eb122
Youssef Magdy <yma.youssef.21@gmail.com>
2024-12-09 14:28:10 +0200

    finished grid design

--- src/components/CountryCardsGrid.tsx
+++ src/components/CountryCardsGrid.tsx
@@ -2,7 +2,7 @@ import CountryCard from "./CountryCard";
 
 function CountryCardsGrid() {
   return (
-    <section className="grid grid-cols-[repeat(auto-fit,_minmax(264px,_1fr))] gap-10 px-4 md:gap-[70px] md:px-[4.75rem] lg:gap-[74px]">
+    <section className="grid grid-cols-[repeat(auto-fit,_264px)] justify-center gap-10 px-4 pb-8 md:justify-between md:gap-x-5 md:gap-y-[74px] md:px-[4.75rem] md:pb-12 lg:gap-x-7">
       <CountryCard />
       <CountryCard />
       <CountryCard />
--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 
 function FormContainer() {
   return (
-    <section className="flex flex-col gap-10 px-4 py-6 md:flex-row md:justify-between md:px-[4.75rem] md:py-12">
+    <section className="flex flex-col gap-10 px-4 pb-8 pt-6 md:flex-row md:justify-between md:px-[4.75rem] md:py-12">
       <SearchBar />
       <DropdownFilter />
     </section>

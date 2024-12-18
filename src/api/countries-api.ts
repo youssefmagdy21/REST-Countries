908d9af88f4fe5bd909ac197167f0da956c2e586
Youssef Magdy <yma.youssef.21@gmail.com>
2024-12-18 23:46:39 +0200

    added functions to fetch countries by name, region

--- src/api/countries-api.ts
+++ src/api/countries-api.ts
@@ -1,11 +1,12 @@
-import { TCountry } from "../types/globalTypes";
+import { TCountry, TRegion, TDetailedCountry } from "../types/globalTypes";
 
 const API_URL = "https://restcountries.com/v3.1";
+const FIELDS = "name,flags,population,region,capital";
+const DETAILED_FIELDS =
+  "name,flags,population,region,capital,subregion,borders,tld,languages,currencies";
 
 export async function getAllCountries(): Promise<TCountry[]> {
-  const res = await fetch(
-    `${API_URL}/all?fields=name,flags,population,region,capital`,
-  );
+  const res = await fetch(`${API_URL}/all?fields=${FIELDS}`);
   if (!res.ok) {
     throw new Error("Somthing went wrong");
   }
@@ -13,6 +14,26 @@ export async function getAllCountries(): Promise<TCountry[]> {
   return res.json();
 }
 
-// export async function getCountriesByRegion(region: TRegion) {}
+export async function getCountriesByRegion(
+  region: TRegion,
+): Promise<TCountry[]> {
+  const res = await fetch(`${API_URL}/region/${region}?fields=${FIELDS}`);
+  if (!res.ok) {
+    throw new Error("Somthing went wrong");
+  }
 
-// export async function getCountryByName(name: string) {}
+  return res.json();
+}
+
+export async function getCountryByName(
+  name: string,
+): Promise<TDetailedCountry[]> {
+  const res = await fetch(
+    `${API_URL}/name/${name}?fullText=true&fields=${DETAILED_FIELDS}`,
+  );
+  if (!res.ok) {
+    throw new Error("Somthing went wrong");
+  }
+
+  return res.json();
+}

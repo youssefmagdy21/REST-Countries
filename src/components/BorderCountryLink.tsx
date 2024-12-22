b022b7738011e569f87261b53ba98a6bc634a4f0
Youssef Magdy <yma.youssef.21@gmail.com>
2024-12-22 20:57:06 +0200

    display borders links dynamically

--- src/components/BorderCountryLink.tsx
+++ src/components/BorderCountryLink.tsx
@@ -1,13 +1,14 @@
+import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
-function BorderCountryLink() {
+function BorderCountryLink({ children }: { children: ReactNode }) {
   return (
     <li className="w-24 rounded-sm bg-light-accent text-center shadow transition-transform duration-300 ease-in-out hover:-translate-y-1">
       <Link
-        to={"/countries/french"}
+        to={`/countries/${children?.toString().toLowerCase()}`}
         className="block py-1.5 text-xs/4 md:text-sm/4"
       >
-        Netherlands
+        {children}
       </Link>
     </li>
   );

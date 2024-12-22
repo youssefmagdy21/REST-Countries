5e984ec996fc0d4c7da4ef675f3320966793c2d4
Youssef Magdy <yma.youssef.21@gmail.com>
2024-12-23 01:58:47 +0200

    added functions to check for empty objects/arrays - fixed some edge case bugs

--- src/util.ts
+++ src/util.ts
@@ -1,17 +1,22 @@
 import { TCurrencies, TLanguages, TNativeName } from "./types/globalTypes";
 
-const getNativeName = (name: TNativeName) =>
-  Object.values(name).reverse()[0].common;
+export const getNativeName = (name: TNativeName) => {
+  const generatedName = Object.values(name).reverse()[0];
 
-const getCurrencies = (currencies: TCurrencies) =>
+  return generatedName.common || generatedName.official;
+};
+
+export const getCurrencies = (currencies: TCurrencies) =>
   Object.values(currencies)
     .map((ele) => ele.name)
     .join(", ");
 
-const getLanguages = (languages: TLanguages) =>
+export const getLanguages = (languages: TLanguages) =>
   Object.values(languages).reverse().join(", ");
 
-const formatPopulation = (population: number) =>
+export const formatPopulation = (population: number) =>
   new Intl.NumberFormat().format(population);
 
-export { getNativeName, getCurrencies, getLanguages, formatPopulation };
+export const isObjectEmpty = (obj: object) => JSON.stringify(obj) === "{}";
+
+export const isArrayEmpty = <T>(arr: Array<T>) => arr.length <= 0;

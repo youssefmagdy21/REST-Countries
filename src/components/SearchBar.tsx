01be15afaefdc76e118ff0603887bed0e6721909
Youssef Magdy <yma.youssef.21@gmail.com>
2025-01-10 00:50:14 +0200

    fixed search by name bugs

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,29 +1,29 @@
 import { useEffect, useState } from "react";
-import { useDebounce } from "../hooks/useDebounce";
 import { useSearchParams } from "react-router-dom";
 import { IoSearchSharp } from "react-icons/io5";
 import { FaRotate } from "react-icons/fa6";
+import debounce from "lodash.debounce";
 
 function SearchBar() {
-  const [searchQuery, setSearchQuery] = useState("");
   const [isSearching, setIsSearching] = useState(false);
-  const debouncedValue = useDebounce(searchQuery);
-  const [, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const nameQuery = searchParams.get("name") ?? "";
   useEffect(() => {
-    setSearchParams((prev) => {
-      if (!debouncedValue) prev.delete("name");
-      else prev.set("name", debouncedValue);
+    const ele = document.getElementById("search") as HTMLInputElement;
+    ele.value = nameQuery;
+  }, [nameQuery]);
 
+  function handleSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const searchValue = e.target.value.trimStart();
+    setSearchParams((prev) => {
+      if (searchValue.length === 0) prev.delete("name");
+      else prev.set("name", searchValue);
       return prev;
     });
 
-    return () => setIsSearching(false);
-  }, [debouncedValue, setSearchParams]);
-
-  function handleSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearchQuery(e.target.value.trimStart());
-    setIsSearching(true);
+    setIsSearching(false);
   }
+  const debouncedSearch = debounce(handleSearchChange, 750);
   return (
     <label className="relative flex h-12 w-full items-center rounded-md bg-accent px-7 font-bold shadow md:h-14 md:w-[480px]">
       <IoSearchSharp className="text-input" />
@@ -33,8 +33,9 @@ function SearchBar() {
         id="search"
         placeholder="Search for a country..."
         className="h-full w-full bg-accent px-6 py-4 text-base/4 placeholder:text-xs placeholder:font-light placeholder:text-input/90 focus:outline-none md:py-[1.125rem]"
-        value={searchQuery}
-        onChange={handleSearchChange}
+        defaultValue={nameQuery}
+        onChange={debouncedSearch}
+        onKeyDown={() => setIsSearching(true)}
       />
       {isSearching && (
         <FaRotate className="absolute right-7 animate-spin text-input" />

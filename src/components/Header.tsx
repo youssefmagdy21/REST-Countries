5db3c42605ea4ae6c6bbe9d5473b9231e48dd4d1
Youssef Magdy <yma.youssef.21@gmail.com>
2025-01-17 00:15:35 +0200

    fixed a bug where box shadow didn't display properly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "../contexts/ThemeContext/ThemeContext";
 function Header() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <header className="bg-accent px-4 shadow md:px-[4.75rem]">
+    <header className="relative bg-accent px-4 shadow md:px-[4.75rem]">
       <div className="flex items-center justify-between py-[1.875rem] md:py-6">
         <h1 className="text-base/5 font-extraBold md:text-xl/8">
           <Link to="/">Where in the world?</Link>
